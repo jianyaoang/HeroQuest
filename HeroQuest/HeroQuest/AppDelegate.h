141fcfe878506dee119cfca742d7fc81dd67dc51
jianyaoang <jianyaoang1@gmail.com>
2014-05-10 13:42:44 -0400

    first commit

---
+++ HeroQuest/HeroQuest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HeroQuest
+//
+//  Created by Jian Yao Ang on 5/10/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeroQuest/HeroQuest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HeroQuest
+//
+//  Created by Jian Yao Ang on 5/10/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
